68bae442332e7608346f3401670ef2c65aafff8c
JustinAJ <justin.aj@hotmail.com>
2014-05-16 22:03:30 -0400

    Striped down to ready for next usage.

--- Tester/Test.cpp
+++ Tester/Test.cpp
@@ -6,44 +6,10 @@
 #include "../Jupiter/Base64.h"
 #include "../Jupiter/InvalidIndex.h"
 
-const char *tString = "ZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMGZOMG";
-const char *tStringL = "zomgzomgzomgzomgzomgzomgzomgzomgzomgzomgzomgzomgzomgzomgzomgzomgzomgzomgzomgzomgzomgzomgzomgzomgzomgzomgzomgzomgzomgzomg";
-
-Jupiter::CStringL lString = "message message message message message message message message message message message message message message message \r\n\r\nmessage message message message message message \rmessage message message\n message message message message message message message message message message message message message message message message message message message message message message message message message message message message message ";
-
-Jupiter::CStringL tString2;
-
-int test()
-{
-	return Jupiter::strlen(tString2.c_str());
-	//return strmatch("ZOMG ?SS CHEEKS! LOLOLOLOL", "ZOMG ASS CHEEKS! LOLOLOLOL");
-}
-
-int test2()
-{
-	return ::strlen(tString2.c_str());
-	//return strmatchi("ZOMG ?SS CHEEKS! LOLOLOLOL", "ZOMG ASS CHEEKS! LOLOLOLOL");
-}
-
-char *randgen(size_t size)
-{
-	char *r = new char[size + 1];
-	r[size] = 0;
-	while (size > 0)
-	{
-		size--;
-		r[size] = rand() % 255 + 1;
-	}
-	return r;
-}
-
 using namespace Jupiter;
 
 int main()
 {
-	printf("%u - %u - %llu" ENDL, INVALID_INDEX, INVALID_INDEX32, INVALID_INDEX64);
-	printf("%u - %u - %llu" ENDL, UINT_MAX, UINT32_MAX, UINT64_MAX);
-
 #if defined _WIN32
 	system("pause");
 #endif // _WIN32
