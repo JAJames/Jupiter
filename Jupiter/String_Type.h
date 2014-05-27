2f2c7e805623ea3d30dd05d14027ba16259fba6c
JustinAJ <justin.aj@hotmail.com>
2014-05-27 15:18:03 -0400

    Moved size variable and many comparative operations to String_Type.

--- Jupiter/String_Type.h
+++ Jupiter/String_Type.h
@@ -18,21 +18,146 @@
 #if !defined _STRING_TYPE_H_HEADER
 #define _STRING_TYPE_H_HEADER
 
-#include "Jupiter.h"
-#include <string> // std::basic_string<T> type
-
 /**
  * @file String_Type.h
  * @brief Provides the basis for String types, of any implementation.
  * Note: Some methods are commented out. This means that they should be implemented, but could not be put declared in this template (return of abstract type).
  */
 
+#include "Jupiter.h"
+#include <string> // std::basic_string<T> type
+#include <cstdarg> // va_list
+#include <cwchar>
+#include <cstdio>
+
 namespace Jupiter
 {
 
+	/**
+	* @brief Provides the basis for String classes by providing implementations for operators, comparative operations, and defining abstract functions.
+	* Note: This is an abstract type.
+	*
+	* @param T Element type which the String will store. Defaults to char.
+	*/
 	template<typename T = char> class String_Type
 	{
 	public:
+
+		/**
+		* @brief Fetches an element from the string.
+		*
+		* @param index Index of the element to return.
+		* @return The element located at the specified index.
+		*/
+		T &get(size_t index) const;
+
+		/**
+		* @brief Returns the number of elements in the String.
+		*
+		* @return Number of elements in the string.
+		*/
+		size_t size() const;
+
+		/**
+		* @brief Returns a pointer to the underlying string of elements.
+		*
+		* @return Pointer to the underlying string of elements.
+		*/
+		const T *ptr() const;
+
+		/**
+		* @brief Checks if the string contains an element with the specified value.
+		*
+		* @param value Value of the element to search for.
+		* @return True if a match is found, false otherwise.
+		*/
+		bool contains(const T &value) const;
+
+		/**
+		* @brief Compares another string against the String.
+		*
+		* @param in String to compare against.
+		* @return 0 if the strings are equal, negative if the first mismatched character is greater in the String, or positive if it's less.
+		*/
+		int compare(const String_Type<T> &in) const;
+		int compare(const std::basic_string<T> &in) const;
+		int compare(const T *in) const;
+		int compare(const T &in) const;
+		int compare(const std::nullptr_t) const;
+
+		/**
+		* @brief Checks if the strings are equal.
+		* Note: Case sensitive.
+		*
+		* @param in String to compare against.
+		* @return True if the contents of the strings are equal, false otherwise.
+		*/
+		bool equals(const String_Type<T> &in) const;
+		bool equals(const std::basic_string<T> &in) const;
+		bool equals(const T *in) const;
+		bool equals(const T &in) const;
+		bool equals(const std::nullptr_t) const;
+
+		/**
+		* @brief Checks if the strings are equal.
+		* Note: Case insensitive. Returns false for any type other than char and wchar_t.
+		*
+		* @param in String to compare against.
+		* @return True if the contents of the strings are equal, false otherwise.
+		*/
+		bool equalsi(const String_Type<T> &in) const;
+		bool equalsi(const std::basic_string<T> &in) const;
+		bool equalsi(const T *in) const;
+		bool equalsi(const T &in) const;
+		bool equalsi(const std::nullptr_t) const;
+
+		/**
+		* @brief Checks if the String matches a wildcard format.
+		* Note: Case sensitive.
+		*
+		* @param format Format that the string is compared against.
+		* @return True if the String matches the wildcard format, false otherwise.
+		*/
+		bool match(const String_Type<T> &format) const;
+		bool match(const std::basic_string<T> &format) const;
+		bool match(const T *format) const;
+
+		/**
+		* @brief Checks if the CString matches a wildcard format.
+		* Note: Case insensitive. Returns false for any type other than char and wchar_t.
+		*
+		* @param format Format that the string is compared against.
+		* @return True if the CString matches the wildcard format, false otherwise.
+		*/
+		bool matchi(const String_Type<T> &format) const;
+		bool matchi(const std::basic_string<T> &format) const;
+		bool matchi(const T *format) const;
+
+		/**
+		* @brief Counts the number of token deliminated words.
+		*
+		* @param whitespace A string of tokens used to deliminate words.
+		* @return Number of words found.
+		*/
+		unsigned int wordCount(const T *whitespace) const;
+
+		/**
+		* @brief Interprets the string as an integer.
+		* Note: This returns 0 on any value string type other than char.
+		*
+		* @param base Base of the string representation.
+		* @return Integer representation of the string.
+		*/
+		int asInt(int base = 0) const;
+
+		/**
+		* @brief Interprets the string as an integer.
+		* Note: This returns 0 on any value string type other than char.
+		*
+		* @param base Base of the string representation.
+		* @return Integer representation of the string.
+		*/
+		unsigned int asUnsignedInt(int base = 0) const;
 
 		/**
 		* @brief Returns a C-Style string version of the String.
@@ -42,68 +167,22 @@ namespace Jupiter
 		virtual const T *c_str() const = 0;
 
 		/**
-		* @brief Returns the number of elements in the String.
+		* @brief Outputs the string to a FILE stream.
 		*
-		* @return Number of elements in the string.
+		* @param out Stream to output to.
+		* @return Number of elements written successfully.
 		*/
-		virtual size_t size() const = 0;
+		size_t print(FILE *out) const;
+		size_t print(std::basic_ostream<T> &out) const;
 
 		/**
-		* @brief Compares another string against the String.
+		* @brief Outputs the string and a newline to a FILE stream
 		*
-		* @param in String to compare against.
-		* @return 0 if the strings are equal, negative if the first mismatched character is greater in the String, or positive if it's less.
+		* @param out Stream to output to.
+		* @param Number of elements written successfully.
 		*/
-		virtual int compare(const String_Type<T> &in) const = 0;
-		virtual int compare(const std::basic_string<T> &in) const = 0;
-		virtual int compare(const T *in) const = 0;
-		virtual int compare(const T in) const = 0;
-
-		/**
-		* @brief Checks if the strings are equal.
-		* Note: Case sensitive.
-		*
-		* @param in String to compare against.
-		* @return True if the contents of the strings are equal, false otherwise.
-		*/
-		virtual bool equals(const String_Type<T> &in) const = 0;
-		virtual bool equals(const std::basic_string<T> &in) const = 0;
-		virtual bool equals(const T *in) const = 0;
-		virtual bool equals(const T in) const = 0;
-
-		/**
-		* @brief Checks if the strings are equal.
-		* Note: Case insensitive. Returns false for any type other than char and wchar_t.
-		*
-		* @param in String to compare against.
-		* @return True if the contents of the strings are equal, false otherwise.
-		*/
-		virtual bool equalsi(const String_Type<T> &in) const = 0;
-		virtual bool equalsi(const std::basic_string<T> &in) const = 0;
-		virtual bool equalsi(const T *in) const = 0;
-		virtual bool equalsi(const T in) const = 0;
-
-		/**
-		* @brief Checks if the String matches a wildcard format.
-		* Note: Case sensitive.
-		*
-		* @param format Format that the string is compared against.
-		* @return True if the String matches the wildcard format, false otherwise.
-		*/
-		virtual bool match(const String_Type<T> &format) const = 0;
-		virtual bool match(const std::basic_string<T> &format) const = 0;
-		virtual bool match(const T *format) const = 0;
-
-		/**
-		* @brief Checks if the CString matches a wildcard format.
-		* Note: Case insensitive. Returns false for any type other than char and wchar_t.
-		*
-		* @param format Format that the string is compared against.
-		* @return True if the CString matches the wildcard format, false otherwise.
-		*/
-		virtual bool matchi(const String_Type<T> &format) const = 0;
-		virtual bool matchi(const std::basic_string<T> &format) const = 0;
-		virtual bool matchi(const T *format) const = 0;
+		size_t println(FILE *out) const;
+		size_t println(std::basic_ostream<T> &out) const;
 
 		/**
 		* @brief Sets the CString's contents based on the format string and input variables.
@@ -113,9 +192,9 @@ namespace Jupiter
 		* @param ... Inputs to match the format specifiers.
 		* @return Number of characters written.
 		*/
-		virtual size_t format(const String_Type<T> &format, ...) = 0;
-		virtual size_t format(const std::basic_string<T> &format, ...) = 0;
-		virtual size_t format(const T *format, ...) = 0;
+		size_t format(const String_Type<T> &format, ...);
+		size_t format(const std::basic_string<T> &format, ...);
+		size_t format(const T *format, ...);
 		virtual size_t vformat(const T *format, va_list args) = 0;
 
 		/**
@@ -126,37 +205,10 @@ namespace Jupiter
 		* @param ... Inputs to match the format specifiers.
 		* @return Number of characters written.
 		*/
-		virtual size_t aformat(const String_Type<T> &format, ...) = 0;
-		virtual size_t aformat(const std::basic_string<T> &format, ...) = 0;
-		virtual size_t aformat(const T *format, ...) = 0;
+		size_t aformat(const String_Type<T> &format, ...);
+		size_t aformat(const std::basic_string<T> &format, ...);
+		size_t aformat(const T *format, ...);
 		virtual size_t avformat(const T *format, va_list args) = 0;
-
-		/**
-		* @brief Counts the number of token deliminated words.
-		*
-		* @param whitespace A string of tokens used to deliminate words.
-		* @return Number of words found.
-		*/
-		virtual unsigned int wordCount(const T *whitespace) const = 0;
-
-		/**
-		* @brief Creates a partial copy of the string.
-		*
-		* @param pos Position in the string to start copying from.
-		* @param length Number of characters to copy.
-		* @return String containing a partial copy of the original string.
-		*/
-		//virtual String_Type<T> substring(size_t pos, size_t length) const = 0;
-		//String_Type<T> substr(size_t pos, size_t length) { return this->substring(pos, length); }
-
-		/**
-		* @brief Creates a partial copy of the string, based on a set of tokens.
-		*
-		* @param pos Position in the string to start copying from.
-		* @param whitespace A string of tokens used to deliminate words.
-		* @return String containing a partial copy of the original string.
-		*/
-		//virtual String_Type<T> getWord(size_t pos, T *whitespace) const = 0;
 
 		/**
 		* @brief Copies the data from the input string to the String.
@@ -188,16 +240,8 @@ namespace Jupiter
 		*/
 		virtual size_t truncate(size_t n) = 0;
 
-		/**
-		* @brief Fetches an element from the string.
-		*
-		* @param index Index of the element to return.
-		* @return The element located at the specified index.
-		*/
-		virtual T &get(size_t index) const = 0;
-
 		/** Access operator */
-		inline T &operator[](size_t index) { return this->get(index); };
+		inline T &operator[](size_t index) { return Jupiter::String_Type<T>::get(index); };
 
 		// Mutative operators.
 		// Note: All extending classes must overload operator= for its own type.
@@ -248,6 +292,7 @@ namespace Jupiter
 
 	protected:
 		T *str; /** Pointer for the underlying string of elements */
+		size_t length; /** Number of representable elements in the string */
 	};
 
 	/** Generic String Type */
@@ -257,5 +302,8 @@ namespace Jupiter
 	typedef String_Type<wchar_t> WStringType;
 
 }
+
+/** Implementation for String_Type. */
+#include "String_Type_Imp.h"
 
 #endif // _STRING_TYPE_H_HEADER
