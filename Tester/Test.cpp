e289f09d5abab0a840e4aed5be1f39d32949bdbc
JustinAJ <justin.aj@hotmail.com>
2014-06-13 00:37:47 -0400

    Removed system-specific code.

--- Tester/Test.cpp
+++ Tester/Test.cpp
@@ -24,8 +24,7 @@ int main()
 {
 	if (goodTests == totalTests) printf("All %u tests succeeded." ENDL, totalTests);
 	else printf("ERROR: Only %u/%u tests succeeded. %u tests failed." ENDL, goodTests, totalTests, totalTests - goodTests);
-#if defined _WIN32
-	system("pause");
-#endif // _WIN32
+	puts("Press any key to continue...");
+	fgetc(stdin);
 	return 0;
 }
