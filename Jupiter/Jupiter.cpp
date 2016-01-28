993a313b81c7fa061d0101bdc2ac101b8b5e2114
JAJames <jessica.aj@outlook.com>
2016-01-27 22:56:32 -0500

    Incremented Jupiter version to 0.18

--- Jupiter/Jupiter.cpp
+++ Jupiter/Jupiter.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2013-2015 Jessica James.
+ * Copyright (C) 2013-2016 Jessica James.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -21,7 +21,7 @@
 extern "C"
 {
 	const char *Jupiter_version = JUPITER_VERSION;
-	const char *Jupiter_copyright = "Copyright (C) 2013-2015 Jessica James. ISC License.";
+	const char *Jupiter_copyright = "Copyright (C) 2013-2016 Jessica James. ISC License.";
 }
 
 const char *Jupiter::version = Jupiter_version;
--- Jupiter/Jupiter.h
+++ Jupiter/Jupiter.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2013-2015 Jessica James.
+ * Copyright (C) 2013-2016 Jessica James.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -36,7 +36,7 @@
 #define JUPITER_API
 #endif // _WIN32
 
-#define JUPITER_VERSION "Jupiter 0.17-dev" /** Version of this program at compile time. */
+#define JUPITER_VERSION "Jupiter 0.18" /** Version of this program at compile time. */
 
 #if defined __cplusplus
 extern "C"
